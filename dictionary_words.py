97ba93959e7269ab5628d67208560fb653a2e1dc
Daniel Hoang <thesubway@gmail.com>
2015-09-24 16:31:35 -0700

    Set dictionary to print out result in sentence format instead

--- dictionary_words.py
+++ dictionary_words.py
@@ -4,10 +4,11 @@ import random
 
 def get_dict_words(num_words):
     if num_words <= 0:
-        return
+        return ""
     my_file = open('words', 'r')
     all_words = []
-    final_result = []
+    final_arr = []
+    final_str = ""
     for line in my_file:
         all_words.append(line)
     for i in range(0, num_words):
@@ -15,10 +16,13 @@ def get_dict_words(num_words):
         current_word = all_words[my_rand_idx]
         # adjust word to remove the \n (last word also has \n)
         adjusted_word = current_word[:-1]
-        final_result.append(adjusted_word)
+        final_arr.append(adjusted_word)
         all_words.remove(current_word)
-    return final_result
+    for each_word in final_arr:
+        final_str += " " + each_word
+    if num_words > 0:
+        final_str = final_str[1:]
+    return final_str
 if __name__ == '__main__':
-    my_arr = get_dict_words(int(sys.argv[1]))
-    for each_word in my_arr:
-        print(each_word)
+    my_sentence = get_dict_words(int(sys.argv[1]))
+    print(my_sentence)
