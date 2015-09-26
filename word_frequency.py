73331c1ae7f1d7a6bdf106ec15dede993ad9a28b
Daniel Hoang <thesubway@gmail.com>
2015-09-25 18:03:53 -0700

    Keep letters, space, or single-quote only

--- word_frequency.py
+++ word_frequency.py
@@ -3,16 +3,14 @@ import string
 
 def histogram(source_text):
     my_file = open(source_text, "r")
-    # my_paragraph = my_file.read()
-    aSet = set(string.punctuation)
-    aSet.remove('-')  # because dashes are allowed
-    # aSet.remove('\\')
+    # note: later code will set uppercase char to lowercase
+    aSet = list("abcdefghijklm' nopqrstuvwxyz")
     adjusted_str = ''
     for line in my_file:
-        adjusted_line = ''.join(ch for ch in line if ch not in aSet)
-        adjusted_line = adjusted_line.replace('-', ' ')
-        adjusted_line = adjusted_line.replace("\n", ' ')
-        adjusted_line = adjusted_line.lower()
+        current_line = line.replace("\n", ' ')
+        current_line = current_line.replace('-', ' ')
+        current_line = current_line.lower()
+        adjusted_line = ''.join(ch for ch in current_line if ch in aSet)
         adjusted_str += adjusted_line
     word_list = adjusted_str.split(' ')
 
@@ -40,6 +38,7 @@ def frequency(word, input_histogram):
 
 if __name__ == '__main__':
     my_dict = histogram("poker.txt")
+    print('data: '+str(my_dict))
     num_unique = unique_words(my_dict)
     print("num unique: "+str(num_unique))
     tested_word = "played"
