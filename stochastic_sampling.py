0fe8ab2afd92b5e0afd215a7ab397f5c2a16c746
Daniel Hoang <thesubway@gmail.com>
2015-09-28 12:30:51 -0700

    Choose random word from histogram without weight

--- stochastic_sampling.py
+++ stochastic_sampling.py
@@ -1,11 +1,17 @@
 import sys
 import word_frequency
+import random
 
 
 def histogram_word(input_histogram):
-    print(input_histogram.keys())
+    input_keys = list(input_histogram.keys())
+    if len(input_keys) == 0:
+        return ""
+    my_num = random.randint(0, len(input_keys)-1)
+    return input_keys[my_num]
 
 if __name__ == '__main__':
     my_file = str(sys.argv[1])
     my_histogram = word_frequency.histogram(my_file)
-    histogram_word(my_histogram)
+    random_word = histogram_word(my_histogram)
+    print(random_word)
