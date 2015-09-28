ecf6bea81a07ef7cabcf8f57a3ea698e36f2732b
Daniel Hoang <thesubway@gmail.com>
2015-09-28 15:40:33 -0700

    Add autocomplete to match to beginning of words

--- extra_sec_2.py
+++ extra_sec_2.py
@@ -1,5 +1,6 @@
 import extra_sec_1
 import dictionary_words
+import sys
 
 
 def anagram_generator(word):
@@ -14,5 +15,16 @@ def anagram_generator(word):
     return real_words
 
 
+def autocomplete_front(str):
+    if len(str) <= 0:
+        return []
+    dict_list = dictionary_words.get_dict_list()
+    valid2 = []
+    for each_word in dict_list:
+        if str in each_word[:len(str)]:
+            valid2.append(each_word)
+    return valid2
+
+
 if __name__ == '__main__':
-    print(anagram_generator('tac'))
+    print(autocomplete_front((sys.argv[1])))
