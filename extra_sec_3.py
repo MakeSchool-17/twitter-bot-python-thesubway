1760c78274138072b47d1db2f2d72ac2b4b631f7
Daniel Hoang <thesubway@gmail.com>
2015-09-28 17:28:58 -0700

    Use tuple method to print alternate histogram

--- extra_sec_3.py
+++ extra_sec_3.py
@@ -1,5 +1,6 @@
 import sys
 import dan_hoang_helper
+import word_frequency
 
 
 def read_histogram(file_name):
@@ -11,5 +12,23 @@ def read_histogram(file_name):
         final_dict[each_arr[0]] = each_arr[1]
     return final_dict
 
+
+def histogram_tuples(source_text):
+    word_list = word_frequency.get_words(source_text)
+    word_list.remove('')
+    word_list.sort()
+    word_tuple = ([], [])
+    for idx, each_str in enumerate(word_list):
+        list_left = word_tuple[0]
+        list_right = word_tuple[1]
+        if each_str in list_left:
+            if each_str == word_list[idx - 1]:
+                list_right[-1] += 1
+        else:
+            list_left.append(each_str)
+            list_right.append(1)
+    print(word_tuple[0])
+    print("and")
+    print(word_tuple[1])
 if __name__ == '__main__':
-    print(read_histogram((sys.argv[1])))
+    histogram_tuples((sys.argv[1]))
