e258e374fcd3d56af82d59154119bfac8b8737b8
Daniel Hoang <thesubway@gmail.com>
2015-09-30 16:08:44 -0700

    Compress functions into single-parameter method

--- compare_histograms.py
+++ compare_histograms.py
@@ -51,25 +51,30 @@ def frequency(word, input_histogram):
         return 0
 
 
-def time_function(num_times, function_name, word, input_histogram):
+def time_function(num_times, function_name, word, input_histogram, input_type):
     start_time = timeit.default_timer()
     dan_hoang_helper.test_results_parameter(num_times, function_name, word, input_histogram)
     elapsed = timeit.default_timer() - start_time
-    print("time: " + str(elapsed))
+    print(input_type + "\t\t: " + str(elapsed))
 
 
-def time_many_funcs(num_times, word, list_histograms, list_funcs):
+def time_many_funcs(num_times, word, list_histograms, list_funcs, input_types):
     for idx, each_function in enumerate(list_funcs):
-        time_function(num_times, each_function, word, list_histograms[idx])
+        time_function(num_times, each_function, word, list_histograms[idx], input_types[idx])
 
-if __name__ == '__main__':
-    histogram_linked_list = histogram("poker.txt")
+
+def compare_structures_using_file(input_file):
+    histogram_linked_list = histogram(input_file)
     frequency_linked = frequency
-    histogram_tuples = extra_sec_3.histogram_tuples("poker.txt")
+    histogram_tuples = extra_sec_3.histogram_tuples(input_file)
     frequency_tuples = extra_sec_3.frequency_tuples
-    histogram_dict = word_frequency.histogram("poker.txt")
+    histogram_dict = word_frequency.histogram(input_file)
     frequency_dict = word_frequency.frequency
 
     three_functions = [frequency_linked, frequency_tuples, frequency_dict]
     three_hgrms = [histogram_linked_list, histogram_tuples, histogram_dict]
-    time_many_funcs(5000, 'not_exist', three_hgrms, three_functions)
+    three_types = ["linked_list", "tuples", "dictionary"]
+    time_many_funcs(5000, 'loremipsum', three_hgrms, three_functions, three_types)
+
+if __name__ == '__main__':
+    compare_structures_using_file("poker.txt")
