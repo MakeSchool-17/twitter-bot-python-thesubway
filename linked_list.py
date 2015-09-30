a5feb34b62b53f31d2014897733e295334fd82e3
Daniel Hoang <thesubway@gmail.com>
2015-09-29 17:22:05 -0700

    Adjust add_node to add nodes in middle of list

--- linked_list.py
+++ linked_list.py
@@ -11,8 +11,13 @@ class Node:
             self.next_node.set_head(input_head)
 
     def add_node(self, new_node):
-        self.next_node = new_node
-        self.next_node.head = self.head
+        new_node.head = self.head
+        if self.next_node is None:
+            self.next_node = new_node
+        else:
+            temp = self.next_node
+            self.next_node = new_node
+            new_node.add_node(temp)
 
 
 class Linked_List:
@@ -29,7 +34,10 @@ class Linked_List:
 if __name__ == '__main__':
     my_node1 = Node(1, None, None)
     my_ll1 = Linked_List(my_node1)
+    my_node3 = Node(3, None, None)
     my_node2 = Node(2, None, None)
+    my_node1.add_node(my_node3)
     my_node1.add_node(my_node2)
     print(my_node1.value)
-    print("2nd node's head: " + str(my_node2.head.value))
+    print(my_node1.next_node.value)
+    print(my_node1.next_node.next_node.value)
