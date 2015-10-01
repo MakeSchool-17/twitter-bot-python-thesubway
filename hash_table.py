24992bd6cef12341f43111d11e6a16f71e3089c5
Daniel Hoang <thesubway@gmail.com>
2015-10-01 11:59:26 -0700

    Add 'update', 'get', 'keys', 'values' functions

--- hash_table.py
+++ hash_table.py
@@ -1,10 +1,10 @@
 class HashTable():
     def __init__(self):
-        self.keys = []
-        self.values = []
+        self.key_list = []
+        self.value_list = []
 
     def index_for_key(self, key):
-        for idx, each_key in enumerate(self.keys):
+        for idx, each_key in enumerate(self.key_list):
             if key == each_key:
                 return idx
         return None
@@ -15,15 +15,36 @@ class HashTable():
         key_idx = self.index_for_key(key)
         if key_idx is None:
             # so key does not exist:
-            self.keys.append(key)
-            self.values.append(val)
+            self.key_list.append(key)
+            self.value_list.append(val)
         else:
-            self.values[key_idx] = val
+            self.value_list[key_idx] = val
         # how to start with
+
+    def update(self, key, val):
+        key_idx = self.index_for_key(key)
+        if key_idx is None:
+            pass
+        else:
+            self.value_list[key_idx] = val
+
+    def get(self, key):
+        key_idx = self.index_for_key(key)
+        return self.value_list[key_idx]
+
+    def keys(self):
+        return self.key_list
+
+    def values(self):
+        return self.value_list
 
 if __name__ == '__main__':
     my_hash = HashTable()
     my_hash.set("one", 1)
-    my_hash.set("one", 11)
-    print(my_hash.keys)
-    print(my_hash.values)
+    my_hash.update("one", 11)
+    my_hash.set("two", 2)
+    my_hash.set("three", 3)
+    my_hash.update("four", 4)  # should do nothing, because "four" does not exist
+    print(my_hash.get("one"))
+    print(my_hash.keys())
+    print(my_hash.values())
