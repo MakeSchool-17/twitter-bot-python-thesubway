19bc70a92e1608f68c68a16174d5aa8e82739f93
Daniel Hoang <thesubway@gmail.com>
2015-10-06 11:50:48 -0700

    Set add_child to allow 2 child nodes for root.

--- heap.py
+++ heap.py
@@ -7,8 +7,12 @@ class Heap:
         return self.root_node
 
     def insert(self, value):
+        new_node = Node(value)
         if self.root_node is None:
             self.root_node = Node(value)
+        else:
+            if value < self.root_node:
+                self.root_node.add_child(new_node)
 
 
 class Node:
@@ -18,3 +22,16 @@ class Node:
         self.parent = None
         self.left_child = None
         self.right_child = None
+
+    def add_child(self, child_node):
+        if self.left_child is None && self.right_child is None:
+            self.left_child = child_node
+        elif self.left_child is not None && self.right_child is None:
+            if child_node.value >= self.left_child.value:
+                self.right_child = child_node
+            else:
+                self.right_child = self.left_child
+                self.left_child = child_node
+
+if __name__ == '__main__':
+    pass
