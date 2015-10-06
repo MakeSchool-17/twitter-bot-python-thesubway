af6cf504ce7228d3ece0cddeda317d4e9ad5c3ce
Daniel Hoang <thesubway@gmail.com>
2015-10-06 15:18:35 -0700

    Add num_nodes, and Heap instance.

--- heap.py
+++ heap.py
@@ -2,6 +2,7 @@ class Heap:
 
     def __init__(self):
         self.root_node = None
+        self.num_nodes = 0
 
     def peek(self):
         return self.root_node
@@ -13,20 +14,21 @@ class Heap:
         else:
             if value < self.root_node:
                 self.root_node.add_child(new_node)
+        self.num_nodes += 1
 
 
 class Node:
 
     def __init__(self, val):
         self.value = val
-        self.parent = None
+        # self.parent = None
         self.left_child = None
         self.right_child = None
 
     def add_child(self, child_node):
-        if self.left_child is None && self.right_child is None:
+        if self.left_child is None and self.right_child is None:
             self.left_child = child_node
-        elif self.left_child is not None && self.right_child is None:
+        elif self.left_child is not None and self.right_child is None:
             if child_node.value >= self.left_child.value:
                 self.right_child = child_node
             else:
@@ -34,4 +36,6 @@ class Node:
                 self.left_child = child_node
 
 if __name__ == '__main__':
-    pass
+    my_heap = Heap()
+    my_heap.insert(1)
+    print(my_heap.root_node.value)
