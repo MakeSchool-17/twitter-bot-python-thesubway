e7e9bafd4003f30479664e5ef56a4d948a6fb0c0
Daniel Hoang <thesubway@gmail.com>
2015-10-05 17:15:56 -0700

    Fix set function in hash table.

--- hash_table.py
+++ hash_table.py
@@ -5,7 +5,7 @@ class HashTable():
     def __init__(self):
         self.key_list = []
         self.value_list = []
-        self.bucket_limit = 20
+        self.bucket_limit = 4
         self.set_up_buckets()
         self.checking_limit = False
 
@@ -28,12 +28,10 @@ class HashTable():
             # store a tuple with key and value
             key_val_pair = (key, val)
             bucket_ll.unshift(key_val_pair)
-            if self.checking_limit == False:
+            if self.checking_limit is False:
                 self.check_limit()
         else:
-            existing_node = bucket_ll.find_node(val)
-            existing_node.value = val
-        # how to start with
+            existing_node.value = (existing_node.value[0], val)
 
     def update(self, key, val):
         hash_val = self.dan_hash(key)
@@ -49,10 +47,12 @@ class HashTable():
             existing_node.value = key_val_pair
 
     def get(self, key):
+        return self.get_from(key, self.value_list)
+
+    def get_from(self, key, val_list):
         hash_val = self.dan_hash(key)
         bucket_idx = hash_val % self.bucket_limit
-        print("bucket_idx = {0} = {1} % {2}".format(bucket_idx, hash_val, self.bucket_limit))
-        bucket_ll = self.value_list[bucket_idx]
+        bucket_ll = val_list[bucket_idx]
         existing_node = bucket_ll.find_node_tuple(key)
         if existing_node is None:
             raise ValueError('Key does not exist')
@@ -63,7 +63,12 @@ class HashTable():
         return self.key_list
 
     def values(self):
-        return self.value_list
+        val_arr = []
+        for each_key in self.key_list:
+            # print key-val pairs:
+            each_val = self.get(each_key)
+            val_arr.append(each_val)
+        return val_arr
 
     def check_limit(self):
         self.checking_limit = True
@@ -73,13 +78,14 @@ class HashTable():
 
     def set_up_buckets(self):
         new_list = []
-        old_list = self.value_list
+        old_keys = self.key_list
+        old_vals = self.value_list
         for each_bucket in range(0, self.bucket_limit):
             each_linked_list = Linked_List()
             new_list.append(each_linked_list)
         self.value_list = new_list
-        for idx, each_key in enumerate(old_list):
-            each_val = self.get(each_key)
+        for idx, each_key in enumerate(old_keys):
+            each_val = self.get_from(each_key, old_vals)
             self.set(each_key, each_val)
 
     def dan_hash(self, input_str):
@@ -99,13 +105,26 @@ class HashTable():
             overall_str += "\n"
         return overall_str
 
+    def print_length_limit(self):
+        print("length: {0}, limit: {1}".format(len(self.key_list), self.bucket_limit))
+
 if __name__ == '__main__':
     my_hash = HashTable()
     my_hash.set("one", 1)
     my_hash.update("one", 11)
     my_hash.set("two", 2)
+    my_hash.print_length_limit()
     my_hash.set("three", 3)
+    my_hash.print_length_limit()
+    my_hash.set("A", "a")
+    my_hash.set("pizza", "food")
+    my_hash.set("avocado", "food")
+    my_hash.print_length_limit()
+    my_hash.set("seven", 7)
+    my_hash.set("thumb", "finger")
     # my_hash.update("four", 4)  # should raise error, because "four" does not exist
-    print(my_hash.get("one"))
+    # print(my_hash.get("one"))
+    # print("keys: " + str(my_hash.keys()))
+    # print("vals: " + str(my_hash.values()))
     print(my_hash)
     print(my_hash.dan_hash('Test hash'))
