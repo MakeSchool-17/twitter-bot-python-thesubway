5d323175d8738a93503f888e8bd79434d7fc2f89
Daniel Hoang <thesubway@gmail.com>
2015-10-08 22:50:17 -0700

    Set parser to create heap of word counts.

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 import re
+import heap
 
 
 def parse_words(source_text):
@@ -16,6 +17,35 @@ def parse_words(source_text):
     token_list = re.split('\s+', adjusted_str)
     return token_list
 
+
+def histogram(source_text):
+    word_list = parse_words(source_text)
+
+    # count occurrence of strings:
+    word_dict = {}
+    for each_str in word_list:
+        if each_str in word_dict:
+            word_dict[each_str] += 1
+        else:
+            word_dict[each_str] = 1
+    if '' in word_dict:
+        del word_dict['']
+    return word_dict
+
+
+def transfer_to_heap(dict):
+    new_heap = heap.Heap()
+    for key in dict:
+        new_heap.insert((key, dict[key]))
+    return new_heap
+
+
+def print_heap(heap):
+    for node in heap.nodes:
+        print(node.value)
+
 if __name__ == '__main__':
-    my_arr = parse_words("meaning_of_good.txt")
-    print(my_arr)
+    my_dict = histogram("meaning_of_good.txt")
+    my_heap = transfer_to_heap(my_dict)
+    print_heap(my_heap)
+    # print(my_dict)
